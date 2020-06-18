d0c7e805f28d25a15571d92045db87c1ecdf1c21
IbnuAhsani <ibnu.muhari@gmail.com>
2020-06-18 13:56:43 +0700

    feat: impl reference stopwords in text preprocessing

--- utils/text_preprocessing.py
+++ utils/text_preprocessing.py
@@ -1,12 +1,16 @@
+import json
 import nltk
 import string
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemover, ArrayDictionary
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
 
-stop_word_factory = StopWordRemoverFactory()
+with open('./data/stop-words.json') as f:
+    reference_stopword_arr = json.load(f)
+
+reference_stopword_dict = ArrayDictionary(reference_stopword_arr)
+stopword_remover = StopWordRemover(reference_stopword_dict)
 stemmer_factory = StemmerFactory()
-stopword = stop_word_factory.create_stop_word_remover()
 stemmer = stemmer_factory.create_stemmer()
 
 
@@ -19,8 +23,7 @@ def generate_tokens(abstract):
     abstract_white_spaces_removed = abstract_lower_case.strip()
     abstract_numbers_removed =  abstract_white_spaces_removed.translate(remove_digits)
     abstract_punctuation_removed = abstract_numbers_removed.translate(remove_punctuations)
-    abstract_stopword_removed = stopword.remove(
-        abstract_punctuation_removed)
+    abstract_stopword_removed = stopword_remover.remove(abstract_punctuation_removed)
     abstract_stemmed = stemmer.stem(abstract_stopword_removed)
     abstract_tokens = nltk.word_tokenize(abstract_stemmed)
 
