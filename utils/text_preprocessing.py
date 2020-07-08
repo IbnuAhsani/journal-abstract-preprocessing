e8bbf6d68047ca6442fb51dc73482f6923d3c37f
IbnuAhsani <ibnu.muhari@gmail.com>
2020-07-08 20:43:38 +0700

    fix: use manual sw removal instead of sastrawi function

--- utils/text_preprocessing.py
+++ utils/text_preprocessing.py
@@ -1,20 +1,16 @@
 import json
 import nltk
 import string
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemover, ArrayDictionary
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
 
-with open('./data/stop-words.json') as f:
-    reference_stopword_arr = json.load(f)
-
-reference_stopword_dict = ArrayDictionary(reference_stopword_arr)
-stopword_remover = StopWordRemover(reference_stopword_dict)
-stemmer_factory = StemmerFactory()
-stemmer = stemmer_factory.create_stemmer()
-
-
 def generate_tokens(abstract):
+
+    with open('./data/stop-words.json') as f:
+        stop_words = json.load(f)
+
+    stemmer_factory = StemmerFactory()
+    stemmer = stemmer_factory.create_stemmer()
 
     remove_digits = str.maketrans(string.digits, ' '*len(string.digits))
     remove_punctuations = str.maketrans(string.punctuation, ' '*len(string.punctuation))
@@ -23,8 +19,10 @@ def generate_tokens(abstract):
     abstract_white_spaces_removed = abstract_lower_case.strip()
     abstract_numbers_removed =  abstract_white_spaces_removed.translate(remove_digits)
     abstract_punctuation_removed = abstract_numbers_removed.translate(remove_punctuations)
-    abstract_stopword_removed = stopword_remover.remove(abstract_punctuation_removed)
-    abstract_stemmed = stemmer.stem(abstract_stopword_removed)
+    abstract_punctuation_removed_list = abstract_punctuation_removed.split()
+    abstract_sw_removed_list = [word for word in abstract_punctuation_removed_list if word not in stop_words]
+    abstract_sw_removed = " ".join(abstract_sw_removed_list)    
+    abstract_stemmed = stemmer.stem(abstract_sw_removed)
     abstract_tokens = nltk.word_tokenize(abstract_stemmed)
 
     final_abstract_tokens = [token for token in abstract_tokens if len(token) >= 3]
