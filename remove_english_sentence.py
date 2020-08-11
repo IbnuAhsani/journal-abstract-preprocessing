10081d7463f6a6588411b76468990b8f78266114
IbnuAhsani <ibnu.muhari@gmail.com>
2020-08-11 14:43:42 +0700

    feat: script to remove english sentence in abstract

---
+++ remove_english_sentence.py
@@ -0,0 +1,60 @@
+import os
+from operator import itemgetter
+from utils import input_output as io
+from langdetect import detect
+
+
+DATASET_DIR = "./data/input/dataset-sample.csv"
+SAVE_PATH = "./data/output/data-sample-english-sentence-removed.csv"
+
+
+def main():
+    csv_data_list = io.read_csv(DATASET_DIR)
+    article_list = []
+
+    csv_header = ['JOURNAL_TITLE', 'ARTICLE_TITLE', 'ARTICLE_ABSTRACT']
+
+    article_list.append(csv_header)
+
+    for i in range(len(csv_data_list)):
+        row = csv_data_list[i]
+
+        journal_title, article_title, article_abstract = itemgetter(
+            'JOURNAL_TITLE', 'ARTICLE_TITLE', 'ARTICLE_ABSTRACT')(row)
+
+        if article_abstract[-1] != '.':
+            article_abstract += '.'
+
+        article_abstract_sentences = article_abstract.split('.')
+
+        j = 0
+        article_english_sentence_removed = ''
+
+        while article_abstract_sentences[j]:
+            article_sentence = article_abstract_sentences[j]
+
+            try:
+                article_sentence_language = detect(article_sentence)
+            except:
+                article_sentence_language = 'error'
+
+            if(article_sentence_language == 'en'):
+                article_abstract_sentences.remove(article_sentence)
+                continue
+
+            if j == 0:
+                article_english_sentence_removed += article_sentence
+            else:
+                article_english_sentence_removed += '. ' + article_sentence
+
+            j += 1
+
+        article = [journal_title, article_title, article_english_sentence_removed]
+
+        article_list.append(article)
+
+    io.save_articles_csv(SAVE_PATH, article_list)
+
+
+if __name__ == '__main__':
+    main()
